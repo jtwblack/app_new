eabee4d23f1fbe30f3032dcc1f988e395687e0d1
JT <thinktankrx@gmail.com>
2017-11-02 16:15:50 -0400

    changes

---
+++ newapp/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+     
+          
+          <h1 className="App-title">Welcome to React</h1>
+     
+        <p className="App-intro">
+
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
